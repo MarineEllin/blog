a39d3db72dbc8bcd2e1bdfe6677ca9c178c72a51
Marine Ellin <marine.ellin@gmail.com>
2023-01-11 17:57:00 +0100

    Mise en place des filtres sur le menu catégories et par date

--- src/index.js
+++ src/index.js
@@ -3,24 +3,41 @@ import "./index.scss";
 
 const articleContainerElement = document.querySelector(".articles-container");
 const categoriesContainerElement = document.querySelector(".categories");
+const selectElement = document.querySelector("select");
+let filter;
+let articles;
+let sortBy = "desc";
 
-const createArticles = (articles) => {
-  const articlesDOM = articles.map((article) => {
-    const articleDOM = document.createElement("div");
-    articleDOM.classList.add("article");
-    articleDOM.innerHTML = `
+selectElement.addEventListener("change", () => {
+  sortBy = selectElement.value;
+  fetchArticle();
+});
+
+const createArticles = () => {
+  const articlesDOM = articles
+    .filter((article) => {
+      if (filter) {
+        return article.category === filter;
+      } else {
+        return true;
+      }
+    })
+    .map((article) => {
+      const articleDOM = document.createElement("div");
+      articleDOM.classList.add("article");
+      articleDOM.innerHTML = `
     <img
               src="${article.img}"
               alt="photo"
             />
             <h2>${article.title}</h2>
             <p class="article-author">${article.author} - ${new Date(
-      article.createdAt
-    ).toLocaleDateString("fr-FR", {
-      day: "2-digit",
-      month: "long",
-      year: "numeric",
-    })}</p>
+        article.createdAt
+      ).toLocaleDateString("fr-FR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      })}</p>
             <p class="article-content">${article.content}
             </p>
             <div class="article-action">
@@ -32,8 +49,8 @@ const createArticles = (articles) => {
               }>Supprimer</button>
             </div>
     `;
-    return articleDOM;
-  });
+      return articleDOM;
+    });
 
   articleContainerElement.innerHTML = "";
   articleContainerElement.append(...articlesDOM);
@@ -71,15 +88,32 @@ const displayMenuCategories = (categoriesArr) => {
   const liElements = categoriesArr.map((categoryElem) => {
     const li = document.createElement("li");
     li.innerHTML = `
-    <li>${categoryElem[0]} (<strong>${categoryElem[1]}</strong>)</li>
+    ${categoryElem[0]} (<strong>${categoryElem[1]}</strong>)
     `;
+    if (categoryElem[0] === filter) {
+      li.classList.add("active");
+    }
+    li.addEventListener("click", () => {
+      if (filter === categoryElem[0]) {
+        filter = null;
+        li.classList.remove("active");
+        createArticles();
+      } else {
+        filter = categoryElem[0];
+        liElements.forEach((li) => {
+          li.classList.remove("active");
+        });
+        li.classList.add("active");
+        createArticles();
+      }
+    });
     return li;
   });
   categoriesContainerElement.innerHTML = "";
   categoriesContainerElement.append(...liElements);
 };
 
-const createMenuCategories = (articles) => {
+const createMenuCategories = () => {
   const categories = articles.reduce((acc, article) => {
     if (acc[article.category]) {
       acc[article.category]++;
@@ -88,21 +122,26 @@ const createMenuCategories = (articles) => {
     }
     return acc;
   }, {});
-  const categoriesArr = Object.keys(categories).map((category) => {
-    return [category, categories[category]];
-  });
+
+  const categoriesArr = Object.keys(categories)
+    .map((category) => {
+      return [category, categories[category]];
+    })
+    .sort((c1, c2) => c1[0].localeCompare(c2[0]));
   displayMenuCategories(categoriesArr);
 };
 
 const fetchArticle = async () => {
   try {
-    const response = await fetch("https://restapi.fr/api/articles");
-    let articles = await response.json();
+    const response = await fetch(
+      `https://restapi.fr/api/articles?sort=createdAt:${sortBy}`
+    );
+    articles = await response.json();
     if (!Array.isArray(articles)) {
       articles = [articles];
     }
-    createArticles(articles);
-    createMenuCategories(articles);
+    createArticles();
+    createMenuCategories();
   } catch (e) {
     console.log("e : ", e);
   }
